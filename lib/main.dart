f3b44cba2ecb08e8cc690c25469acf44f1aa9c6e
Carlos Caravaca <ccaravac@everis.com>
2020-05-25 12:11:48 +0100

    Swich working ConnectionState

--- lib/main.dart
+++ lib/main.dart
@@ -86,3 +86,80 @@ class _ChangeColorState extends State<ChangeColor> {
     );
   }
 }
+
+// Working with swith and case
+// class ChangeColor extends StatefulWidget {
+//   @override
+//   _ChangeColorState createState() => _ChangeColorState();
+// }
+
+// class _ChangeColorState extends State<ChangeColor> {
+//   final colorStream = new StreamController<Color>();
+
+//   int counter = -1;
+
+//   final List<Color> colors = [Colors.blue, Colors.red, Colors.yellow];
+
+//   @override
+//   void dispose() {
+//     colorStream.close();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: StreamBuilder(
+//           stream: colorStream.stream,
+//           builder: (BuildContext context, AsyncSnapshot<Color> snapshot) {
+//             switch (snapshot.connectionState) {
+//               case ConnectionState.waiting:
+//                 return Center(child: CircularProgressIndicator());
+//                 break;
+//               case ConnectionState.active:
+//                 return Container(
+//                   height: 200,
+//                   width: 200,
+//                   color: snapshot.data,
+//                 );
+//                 break;
+//               case ConnectionState.done:
+//                 return Center(child: Text('Fin'));
+//                 break;
+//               default:
+//                 if (snapshot.hasError) {
+//                   return Text('Error has occurred');
+//                 } else if (!snapshot.hasData) {
+//                   return Center(child: CircularProgressIndicator());
+//                 } else {
+//                   var result = snapshot.data;
+//                   if (result != null) {
+//                     return Container(
+//                       height: 200,
+//                       width: 200,
+//                       color: snapshot.data,
+//                     );
+//                   } else {
+//                     return Text('snapshot.data = null');
+//                   }
+//                 }
+
+//                 break;
+//             }
+//           },
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.add_box),
+//           onPressed: () {
+//             counter++;
+//             if (counter < colors.length) {
+//               colorStream.sink.add(colors[counter]);
+//             } else {
+//               colorStream.close();
+//             }
+//           }),
+//     );
+//   }
+// }
